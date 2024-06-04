44182a8b3146f45afa081355c72d2247cc04f40a
IHsuanHu <ihsuanhu@gmail.com>
2024-06-04 00:13:49 -0400

    update env variable

--- urlShortener/src/environments/environment.prod.ts
+++ urlShortener/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: process.env["API_BASE_URL"] || 'https://urlshortener-iqwe.onrender.com'  // 默認值用於生產環境
+    apiUrl: 'https://urlshortener-iqwe.onrender.com'  // 默認值用於生產環境
   };
   
