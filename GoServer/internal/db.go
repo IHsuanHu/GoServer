59df7676c0671175efeed425e914eddfb560d2ba
IHsuanHu <ihsuanhu@gmail.com>
2024-06-04 01:33:09 -0400

    update db info

--- GoServer/internal/db.go
+++ GoServer/internal/db.go
@@ -22,7 +22,7 @@ func init() {
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbname)
 	db, err = sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
